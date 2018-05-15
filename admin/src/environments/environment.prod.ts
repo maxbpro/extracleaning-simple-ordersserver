e08d4b38c5c59a85cb79a789c029075c89e72363
MaxB <maxbpro2009@gmail.com>
2018-05-15 13:44:40 +0300

    Updated production url

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverEndpoint: 'http://localhost:8080'
+  serverEndpoint: 'http://xcleaner.ru:8080'
 };
--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverEndpoint: 'http://localhost:8080/api/v1'
+  serverEndpoint: 'http://xcleaner.ru:8080/api/v1'
 };
